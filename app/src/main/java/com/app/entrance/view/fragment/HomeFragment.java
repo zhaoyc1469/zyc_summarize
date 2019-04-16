35a08984348b3ff319bfd6eaf8b02ff4a0f75b24
zhaoyc <yuchi.zhao@sdbean.com>
2019-04-16 14:15:12 +0800

    test

--- app/src/main/java/com/app/entrance/view/fragment/HomeFragment.java
+++ app/src/main/java/com/app/entrance/view/fragment/HomeFragment.java
@@ -6,16 +6,16 @@ import android.view.ViewGroup;
 
 import com.app.entrance.BR;
 import com.app.entrance.R;
-import com.app.entrance.databinding.ActivityHomeBinding;
+import com.app.entrance.databinding.FragmentHomeBinding;
 import com.app.entrance.viewModel.HomeFrgViewModel;
 import com.app.frame.base.BaseFragment;
 
-public class HomeFragment extends BaseFragment<ActivityHomeBinding, HomeFrgViewModel> {
+public class HomeFragment extends BaseFragment<FragmentHomeBinding, HomeFrgViewModel> {
 
 
     @Override
     public int initContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return R.layout.activity_home;
+        return R.layout.fragment_home;
     }
 
 
