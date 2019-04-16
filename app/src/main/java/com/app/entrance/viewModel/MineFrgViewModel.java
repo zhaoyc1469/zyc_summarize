297acd02aa8c127037dfbdf08b0d84cffa17744d
zhaoyc <yuchi.zhao@sdbean.com>
2019-04-16 18:04:08 +0800

    test

--- app/src/main/java/com/app/entrance/viewModel/MineFrgViewModel.java
+++ app/src/main/java/com/app/entrance/viewModel/MineFrgViewModel.java
@@ -2,8 +2,10 @@ package com.app.entrance.viewModel;
 
 import com.app.entrance.contract.IMineViewModel;
 import com.app.frame.base.BaseViewModel;
+import com.app.frame.binding.command.BindingCommand;
 import com.app.frame.contract.IViewModel;
 import com.app.entrance.model.MineModel;
+import com.mylibrary.magiccamera.view.activity.ScreenTestActivity;
 
 public class MineFrgViewModel extends BaseViewModel<MineModel> implements IMineViewModel {
 
@@ -11,4 +13,8 @@ public class MineFrgViewModel extends BaseViewModel<MineModel> implements IMineV
     protected MineModel initModel(IViewModel ViewModel) {
         return new MineModel(this);
     }
+
+
+
+    public BindingCommand cameraClick = new BindingCommand(() -> startActivity(ScreenTestActivity.class));
 }
